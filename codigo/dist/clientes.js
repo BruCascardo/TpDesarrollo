6aaa593674bb927fbc987211b4104898e1c483b0
Bruno <brucascardo@gmail.com>
2024-05-24 10:07:10 -0300

    cambios

---
+++ codigo/dist/clientes.js
@@ -0,0 +1,10 @@
+export class Cliente {
+    constructor(nombre, apellido, dni, email, fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
+//# sourceMappingURL=clientes.js.map
